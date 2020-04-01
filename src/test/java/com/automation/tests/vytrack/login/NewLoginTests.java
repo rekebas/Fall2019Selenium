51f69dacefed15a698846af2dd71964045e35e5b
rekebas <aekebas@hotmail.com>
2020-04-01 14:52:56 -0500

    first commit

---
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -0,0 +1,43 @@
+package com.automation.tests.vytrack.login;
+
+import com.automation.pages.LoginPage;
+import com.automation.tests.vytrack.AbstractTestBase;
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewLoginTests extends AbstractTestBase {
+
+
+    @Test
+    public void verifyPageTitle(){
+        //test --> ExtentTest object
+        //we must add to every test at the beginning
+        //test = report.createTest("Test name");
+        test = report.createTest("Verify page title");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        //like sysyem.out, but it goes to report as well
+        test.info("Login as store manager"); //log some steps
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboar");
+        //if assertion passed, it will set test status in report to passed
+        test.pass("Page title Dashboard was verified");
+    }
+
+    /**
+     * Enter wrong credentials and verify warning message
+     *
+     */
+
+    @Test
+    public void verifyWarningMessage(){
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("wrong", "wrong");
+        Assert.assertEquals(loginPage.getWarningMessageText(), "Invalid user name or password.");
+        //take a screenshot
+        BrowserUtils.getScreenshot("warning_message");
+
+        test.pass("Warning message is displayed");
+    }
+}
